9ae0e4192bb0c2399ad2e6f7c7d6065b6cb968bf
yogesh jha <yogeshjha123456@gmail.com>
2025-02-23 22:23:15 +0530

    new

--- linkedlist/doublelinkedlist.cpp
+++ linkedlist/doublelinkedlist.cpp
@@ -17,21 +17,37 @@ class Node{
 };
 
 int main(){
-     Node *head=NULL;
-     //inset at start
-     if(head==NULL){ //if head is null
-        head=new  Node(5);
+     Node *head=NULL,*tail=NULL;
+
+     int arr[5]={1,2,3,4,5};
+     //copy array data into linkedlist
+     for(int i=0;i<5;i++){
+        if(head==NULL){
+            head=new Node(arr[i]);
+            tail=head;
+        }
+        else{
+            Node * temp=new Node(arr[i]);
+            tail->next=temp;
+            temp->prev=tail;
+            tail=temp;
+        }
      }
-     else{            //if node is present
-        Node * temp=new Node(5);
-        temp->next=head;
-        head->prev=temp;
-        head=temp;
-     }
+
+    //  //inset at start
+    //  if(head==NULL){ //if head is null
+    //     head=new  Node(5);
+    //  }
+    //  else{            //if node is present
+    //     Node * temp=new Node(5);
+    //     temp->next=head;
+    //     head->prev=temp;
+    //     head=temp;
+    //  }
 
      Node *temp=head;
      while(temp){
-        cout<<temp->data;
+        cout<<temp->data<<" ";
         temp=temp->next;
      }
 }
