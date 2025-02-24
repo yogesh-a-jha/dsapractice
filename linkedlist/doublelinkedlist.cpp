7ae5cead432c200323e1ec76e96705af7a5edd37
yogesh jha <yogeshjha123456@gmail.com>
2025-02-24 22:33:09 +0530

    new

--- linkedlist/doublelinkedlist.cpp
+++ linkedlist/doublelinkedlist.cpp
@@ -16,23 +16,43 @@ class Node{
 
 };
 
+Node * dll(int arr[],int i,int size,Node *back){
+    if(i==size){
+        return NULL;
+    }
+    Node *temp=new Node(arr[i]);
+    temp->prev=back;
+    temp->next=dll(arr,i+1,size,temp);
+
+    return temp;
+
+
+}
+
 int main(){
      Node *head=NULL,*tail=NULL;
 
      int arr[5]={1,2,3,4,5};
-     //copy array data into linkedlist
-     for(int i=0;i<5;i++){
-        if(head==NULL){
-            head=new Node(arr[i]);
-            tail=head;
-        }
-        else{
-            Node * temp=new Node(arr[i]);
-            tail->next=temp;
-            temp->prev=tail;
-            tail=temp;
-        }
-     }
+
+
+     //create with recurssion
+
+     head=dll(arr,0,5,NULL);
+
+
+    //  //copy array data into linkedlist
+    //  for(int i=0;i<5;i++){
+    //     if(head==NULL){
+    //         head=new Node(arr[i]);
+    //         tail=head;
+    //     }
+    //     else{
+    //         Node * temp=new Node(arr[i]);
+    //         tail->next=temp;
+    //         temp->prev=tail;
+    //         tail=temp;
+    //     }
+    //  }
 
     //  //inset at start
     //  if(head==NULL){ //if head is null
