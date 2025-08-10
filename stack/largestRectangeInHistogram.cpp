0c7aa523397f5fcafd93c01885a272c1fb0643cb
yogesh jha <yogeshjha123456@gmail.com>
2025-08-10 23:46:20 +0530

    new

---
+++ stack/largestRectangeInHistogram.cpp
@@ -0,0 +1,46 @@
+class Solution {
+  public:
+    int getMaxArea(vector<int> &arr) {
+        // Your code here
+         int n= arr.size();
+        vector<int>right(n);
+        vector<int>left(n);
+        stack<int>st;
+
+        //for next smallest in the right
+        for(int i=0;i<n;i++){
+            while(!st.empty()&&arr[st.top()]>arr[i]){
+                right[st.top()]=i;
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            right[st.top()]=n;
+            st.pop();
+        }
+
+        //for next smallest in the left
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()&&arr[st.top()]>arr[i]){
+                left[st.top()]=i;
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            left[st.top()]=-1;
+            st.pop();
+        }
+
+        //calculating height
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=max(ans,arr[i]*(right[i]-left[i]-1));
+        }
+
+        return ans;
+    }
+};
