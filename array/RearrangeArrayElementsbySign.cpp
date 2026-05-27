6c1253fee68c8ae672d80e6aae14ed3a7930dd8b
yogesh jha <yogeshjha123456@gmail.com>
2026-05-27 19:38:26 +0530

    Rearrange Array Elements by Sign

---
+++ array/RearrangeArrayElementsbySign.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>pos;
+        vector<int>neg;
+        for(int i = 0;i<n;i++){
+            if(nums[i]>=0){
+                pos.push_back(nums[i]);
+            }
+            else{
+                neg.push_back(nums[i]);
+            }
+        }
+
+        vector<int>ans;
+        for(int i = 0;i<n/2;i++){
+            ans.push_back(pos[i]);
+            ans.push_back(neg[i]);
+        }
+
+        return ans;
+    }
+};
