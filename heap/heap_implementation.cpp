9b3be14f5799d507a01199c75ff1daec8a82bb85
yogesh jha <yogeshjha123456@gmail.com>
2026-06-09 22:02:53 +0530

    heap delete

--- heap/heap_implementation.cpp
+++ heap/heap_implementation.cpp
@@ -29,11 +29,49 @@ class MaxHeap {
         cout<<arr[index]<<" is inserted into the heap..."<<endl;
     }
     
+    void Heapify(int index){
+        int largest = index;
+        int left = 2*index +1;
+        int right = 2*index +2;
+        
+        if(left<size && arr[left]>arr[largest]){
+            largest = left;
+        }
+        if(right<size && arr[right]>arr[largest]){
+            largest = right;
+        }
+        
+        if(largest!=index){
+            swap(arr[index],arr[largest]);
+            Heapify(largest);
+        }
+        
+        
+    }
+    
+    void Delete(){
+        if(size == 0){
+            cout<<"Heap is empty!\n";
+            return;
+        }
+        
+        cout<<arr[0]<<" deleted from the heap!\n";
+        arr[0] = arr[size-1];
+        size--;
+        
+        if(size == 0){
+            return;
+        }
+        
+        Heapify(0);
+    }
+    
     void print(){
         for(int i=0;i<size;i++){
             cout<<arr[i]<<" ";
         }
     }
+    
     
     
 };
@@ -44,8 +82,9 @@ int main() {
     H1.insert(4);
     H1.insert(14);
     H1.insert(11);
-    H1.insert(7);
-    H1.insert(9);
+    H1.print();
+    cout<<endl;
+    H1.Delete();
     H1.print();
     
 
